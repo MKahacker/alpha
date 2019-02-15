d098b326f36dd629481375107274c4bf159be3f5
makk <mkahack@hawk.iit.edu>
2019-02-15 16:26:40 -0600

    Migrating changes including adding new column   $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_15_220931) do
+ActiveRecord::Schema.define(version: 2019_02_15_222431) do
 
   create_table "days", force: :cascade do |t|
     t.string "date"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_02_15_220931) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "nutritions", force: :cascade do |t|
