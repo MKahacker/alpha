70cfffa4f6c12c1e56c033bb4ce47f0ed32e8acc
makk <mkahack@hawk.iit.edu>
2019-02-17 10:55:23 -0600

    Adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Food.destroy_all
+Nutrition.destroy_all
+Day.destroy_all
+
+@day1 = Day.create(
+  date:Time.new.to_s,
+  calories: 40,
+  water: 3.0
+)
+
+@food = Food.create(
+  name: "Almond Milk",
+  category: "Nondairy Milk",
+  servings: 1.0,
+  day_id: @day1
+)
+
+@nut = Nutrition.create(
+  calories: 40,
+  carbs: 1,
+  protein: 3,
+  fats: 3,
+  food_id: @food
+)
