10d53dae74134042c8490b4c18dccbc0dad4f848
makk <mkahack@hawk.iit.edu>
2019-02-23 21:43:12 -0600

    Added validation for category

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   validates :name, presence: {message: "Name connot be blank"}
+  validates :category, presence: {message: "Category shouldn't be black"}
   has_one :nutritions
   has_one :days, through: :servings
 end
