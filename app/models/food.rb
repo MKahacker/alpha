f002f3d1cdb1841d6b2f81866997154c606536f8
makk <mkahack@hawk.iit.edu>
2019-03-03 13:27:14 -0600

    added validation and tests for food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   validates :name, presence: {message: "Name connot be blank"}
-  validates :category, presence: {message: "Category shouldn't be black"}
+  validates :category, presence: {message: "Category shouldn't be blank"}
+  validates :calories, presence: {message: "Calories shouldn't be blank"}, numericality: {greater_than_or_equal_to: 0, only_integer: true}
   has_one :day, through: :serving
 end
--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -3,9 +3,34 @@ require 'test_helper'
 class FoodTest < ActiveSupport::TestCase
   setup do
     @no_id = Food.create()
+    @name = "Peanuts"
+    @category = "nuts"
+    @calories = 200
   end
-  test "if validates" do
+  test "invalid if no parameters" do
      refute @no_id.valid?
      assert @no_id.errors[:name].any?
+  end
+  test "invalid when name missing" do
+    refute Food.create(category: @category, calories: @calories).valid?
+  end
+  test "invalid when category missing" do
+    refute Food.create(name:@name, calories: @calories).valid?
+  end
+  test "check invalid if calories left blank" do
+    refute Food.create(name:@name, category: @category).valid?
+  end
+  test "check that if calories are negative invalid" do
+    refute Food.create(name:@name,  category: @category,calories: -200).valid?
+  end
+  test "check that invalid if calories not number" do
+    refute Food.create(name:@name,  category: @category,calories: "two hundred").valid?
+    refute Food.create(name:@name,  category: @category,calories: true).valid?
+  end
+  test "check that invalid if calories are decimal" do
+    refute Food.create(name:@name,  category: @category,calories: 7.8).valid?
+  end
+  test "check valid when calories, name, and category included" do
+    assert Food.create(name:@name,  category: @category,calories: 200).valid?
   end
 end
