cc1fe8138acb8b52c3eabd427d11b8f7897fe2d5
makk <mkahack@hawk.iit.edu>
2019-03-03 12:50:01 -0600

    Adding tests for serving

--- test/models/serving_test.rb
+++ test/models/serving_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ServingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @serv = servings(:bserving)
+  end
+
+  test "check that there is two serving in test database" do
+    assert_equal 2, Serving.all.count
+  end
+
+  test "test that serving cannot be left blank" do
+   refute Serving.create().valid?
+  end
+
+  test "test that adding serving does make it valid" do
+    assert Serving.create(serving: 1.0).valid?
+  end
+
+  test "check that count gets updated when adding new serving" do
+    @new_serv = Serving.create(serving:1.0)
+    @new_serv.save
+    assert_equal 3, Serving.all.count
+  end
 end
