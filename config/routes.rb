cc007518d523f4f8f490d917214b26149123ff5f
makk <mkahack@hawk.iit.edu>
2019-03-03 12:19:53 -0600

    Adding index route for my controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :servings, except: [:destroy]
+  get "/servings", to: "servings#index"
 end
