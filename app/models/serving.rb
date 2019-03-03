68c7d0ef56c66f6449b5ffc98d3ac31757f1a025
makk <mkahack@hawk.iit.edu>
2019-03-03 13:08:56 -0600

    Adding numericality check to servings model

--- app/models/serving.rb
+++ app/models/serving.rb
@@ -1,5 +1,5 @@
 class Serving < ApplicationRecord
-  validates :serving, presence: {message: "Cannot leave servings blank."}
+  validates :serving, presence: {message: "Cannot leave servings blank."}, numericality: {greater_than_or_equal_to: 0.0}
   belongs_to :food, optional: true
   belongs_to :day, optional: true
 end
