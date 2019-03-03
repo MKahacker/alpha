2e0af1c3f0364ff2a924af3deff3906da4dbaf5d
makk <mkahack@hawk.iit.edu>
2019-03-03 11:55:41 -0600

    Fixed the plural references in has_one and belongs_to

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,5 @@
 class Food < ApplicationRecord
   validates :name, presence: {message: "Name connot be blank"}
   validates :category, presence: {message: "Category shouldn't be black"}
-  has_one :nutritions
-  has_one :days, through: :servings
+  has_one :day, through: :serving
 end
--- app/models/serving.rb
+++ app/models/serving.rb
@@ -1,5 +1,5 @@
 class Serving < ApplicationRecord
   validates :serving, presence: {message: "Cannot leave servings blank."}
-  belongs_to :foods, optional: true
-  belongs_to :days, optional: true
+  belongs_to :food, optional: true
+  belongs_to :day, optional: true
 end
