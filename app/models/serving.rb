b3268687e6e1efc77b6e1e51470d3f99bfe621a9
makk <mkahack@hawk.iit.edu>
2019-03-13 16:39:00 -0500

    Added getters and setters for food_name

--- app/models/serving.rb
+++ app/models/serving.rb
@@ -2,4 +2,12 @@ class Serving < ApplicationRecord
   validates :serving, presence: {message: "Cannot leave servings blank."}, numericality: {greater_than_or_equal_to: 0.01}
   belongs_to :food, optional: true
   belongs_to :day, optional: true
+
+  def food_name
+    self.food ? self.food.name : nil
+  end
+
+  def food_name=(name)
+    self.food = Food.find_or_create_by(name: name)
+  end
 end
