9420fbfb1855ee2d30bb8927dd23fc8f2bbbba72
makk <mkahack@hawk.iit.edu>
2019-03-14 18:01:19 -0500

    Added new routes for foods controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :servings
   root "servings#index"
+  get "/foods/new", to: "foods#new"
+  post "/foods", to: "foods#create"
+  #TODO :- Work on update
+  # put "/foods/:id", to: "foods#update"
+  # patch "/foods/:id", to: "foods#update"
 end
