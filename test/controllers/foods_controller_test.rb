173362a2ca06de64bb2717782c495b3c5645e0a7
makk <mkahack@hawk.iit.edu>
2019-03-14 18:02:15 -0500

    Created test for food controller: :

--- test/controllers/foods_controller_test.rb
+++ test/controllers/foods_controller_test.rb
@@ -2,47 +2,51 @@ require 'test_helper'
 
 class FoodsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @food = foods(:one)
+    @food = foods(:Banana)
   end
 
-  test "should get index" do
-    get foods_url
-    assert_response :success
+  test "Food objects created" do
+    assert_equal 2, Food.count
   end
 
   test "should get new" do
-    get new_food_url
+    get "/foods/new"
     assert_response :success
   end
 
   test "should create food" do
     assert_difference('Food.count') do
-      post foods_url, params: { food: {  } }
+      post "/foods", params: { food: {name: "Roasted Chicken", category: "Poultry", calories: 240, protein: 24, fat: 16, carbohydrates:1 } }
     end
 
-    assert_redirected_to food_url(Food.last)
+    assert_redirected_to servings_url
   end
 
-  test "should show food" do
-    get food_url(@food)
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get foods_url
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get edit_food_url(@food)
-    assert_response :success
-  end
+  # test "should show food" do
+  #   get food_url(@food)
+  #   assert_response :success
+  # end
 
-  test "should update food" do
-    patch food_url(@food), params: { food: {  } }
-    assert_redirected_to food_url(@food)
-  end
+#  test "should get edit" do
+#    get edit_food_url(@food)
+#    assert_response :success
+#  end
 
-  test "should destroy food" do
-    assert_difference('Food.count', -1) do
-      delete food_url(@food)
-    end
+  # test "should update food" do
+  #   patch "/foods/" + @food.id.to_s, params: { food: {carbohydrates: 8  } }
+  #   assert_redirected_to servings_url
+  # end
 
-    assert_redirected_to foods_url
-  end
+  # test "should destroy food" do
+  #   assert_difference('Food.count', -1) do
+  #     delete food_url(@food)
+  #   end
+  #
+  #   assert_redirected_to foods_url
+  # end
 end
